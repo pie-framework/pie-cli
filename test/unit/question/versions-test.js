7ad5b26b7fee88945335be5aacd897e885914f4f
edeustace <ed.eustace@gmail.com>
2016-09-28 12:41:27 +0100

    fix name

--- test/unit/question/versions-test.js
+++ test/unit/question/versions-test.js
@@ -7,11 +7,11 @@ describe('versions', () => {
   describe('rangeSpansMajorVersion', () => {
 
     it('returns true for a major version range', () => {
-      new Versions(['~1.0.0', '~2.0.0']).isBreakingRange.should.equal(true);
+      new Versions(['~1.0.0', '~2.0.0']).rangeSpansMajorVersion.should.equal(true);
     });
     
     it('returns false for range within the major version', () => {
-      new Versions(['~1.2', '1.2.10']).isBreakingRange.should.equal(false);
+      new Versions(['~1.2', '1.2.10']).rangeSpansMajorVersion.should.equal(false);
     });
   });
 });
