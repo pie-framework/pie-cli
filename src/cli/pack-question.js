9273482ddf7d0e61ea913843bb252d93bc9ac770
edeustace <ed.eustace@gmail.com>
2016-10-05 15:58:11 +0100

    updates

--- src/cli/pack-question.js
+++ src/cli/pack-question.js
@@ -1,11 +1,10 @@
-import * as packer from '../question/packer';
-import { fileLogger } from '../log-factory';
+import { buildLogger } from '../log-factory';
 import Question from '../question';
-import Packer from '../question/new-packer';
+import Packer, {DEFAULTS} from '../question/packer';
 import path from 'path';
 import FrameworkSupport from '../framework-support';
 
-const logger = fileLogger(__filename);
+const logger = buildLogger();
 
 var marked = require('marked');
 var TerminalRenderer = require('marked-terminal');
@@ -26,20 +25,20 @@ export let usage = marked(`
 Generate some javascript for use in rendering the question.
 
 It generates 2 javascript files: 
- * \`${packer.DEFAULTS.pieJs}\` - contains all the logic for rendering, includes the individual pies, a pie-player definition and ??
- * \`${packer.DEFAULTS.controllersJs}\` - contains a map of pie names to their controllers, exports the map to either \`window\` or \`exports\`.
+ * \`${DEFAULTS.pieJs}\` - contains all the logic for rendering, includes the individual pies, a pie-player definition and ??
+ * \`${DEFAULTS.controllersJs}\` - contains a map of pie names to their controllers, exports the map to either \`window\` or \`exports\`.
 
 > Note: This doesn't generate the final question for you. To do that you'll need to create the final html page, include the 2 js files above, and use a controller that can interact with the controller-map.js file. See [pie-docs](http://pielabs.github.io/pie-docs) for more infomation.
 
 ### Options
-  \`--support\` - an npm module or js file to load to add support for a build type.
+  \`--support\` - an npm module or js file to load to add support for a build type. (NOT READY YET - don't use)
   \`--dir\` - the relative path to a directory to use as the root. This should contain \`config.json\` and \`index.html\` (default: the current working directory)
-  \`--configFile\` - the name of the pie data file - default \`${packer.DEFAULTS.configFile}\`
-  \`--keepBuildAssets\` - keep supporting build assets (like node_modules etc) - default \`${packer.DEFAULTS.keepBuildAssets}\`
-  \`--dependenciesFile\` - the name of the dependencies file (to be removed) - default \`${packer.DEFAULTS.dependenciesFile}\`
-  \`--buildExample\` - build an example? - default \`${packer.DEFAULTS.buildExample}\`
-  \`--markupFile\` - if building an example - the name of the html file with the layout for the question. - default \`${packer.DEFAULTS.markupFile}\`
-  \`--exampleFile\` - if building an example - the name of the generated example html file.  - default \`${packer.DEFAULTS.exampleFile}\`
+  \`--configFile\` - the name of the pie data file - default \`${DEFAULTS.configFile}\`
+  \`--keepBuildAssets\` - keep supporting build assets (like node_modules etc) - default \`${DEFAULTS.keepBuildAssets}\`
+  \`--dependenciesFile\` - the name of the dependencies file (to be removed) - default \`${DEFAULTS.dependenciesFile}\`
+  \`--buildExample\` - build an example? - default \`${DEFAULTS.buildExample}\`
+  \`--markupFile\` - if building an example - the name of the html file with the layout for the question. - default \`${DEFAULTS.markupFile}\`
+  \`--exampleFile\` - if building an example - the name of the generated example html file.  - default \`${DEFAULTS.exampleFile}\`
 ### Examples
 \`\`\`shell
 pie-cli pack-question --dir ../path/to/dir 
--- src/question/packer.js
+++ src/question/packer.js
@@ -1,15 +1,105 @@
 import NpmDir from '../npm/npm-dir';
-import Config from '../question/config';
-import fs from 'fs-extra';
-import path from 'path';
 import * as elementBundle from '../code-gen/element-bundle';
 import * as controllerMap from '../code-gen/controller-map';
 import * as markupExample from '../code-gen/markup-example';
-import _ from 'lodash';
+import _ from 'lodash'
+import { buildLogger } from '../log-factory';
 
-import {fileLogger} from '../log-factory'; 
+let logger = buildLogger();
 
-let logger = fileLogger(__filename);
+/**
+ * A question packer.
+ * > packing means building 2 files: pie.js and controllers.js
+ * 
+ * pie pq --support ./node_modules/pie-vue-support 
+ */
+export default class Packer {
+  constructor(question, frameworkSupport) {
+    logger.silly('new Packer');
+    this._question = question;
+    this._npmDir = new NpmDir(this._question.dir);
+    this._frameworkSupport = frameworkSupport;
+  }
+
+  clean(opts) {
+    logger.debug('[clean]', opts);
+    opts = _.extend({}, DEFAULTS, opts);
+    let root = this._question.dir;
+    return this._npmDir.clean()
+      .then(() => elementBundle.clean(root, opts.pieJs))
+      .then(() => controllerMap.clean(root, opts.controllersJs))
+      .then(() => markupExample.clean(root, opts.exampleFile));
+  }
+
+  pack(opts) {
+    logger.silly('[pack]', opts);
+
+    opts = _.extend({}, DEFAULTS, opts);
+
+    logger.silly('[pack] opts: ', opts);
+
+    let npmDependencies = _.extend({}, DEFAULT_DEPENDENCIES, this._question.npmDependencies);
+    
+    let pieNames = _.keys(npmDependencies);
+
+    logger.debug('npm dependencies: ', JSON.stringify(npmDependencies));
+
+
+    let buildElementBundle = (supportConfig) => {
+
+      //TODO: This has been externalised - will prob change anyway w/ new controller build.
+      let pieController = {
+        key: 'pie-controller',
+        initSrc: `
+        import Controller from 'pie-controller';
+        window.pie = window.pie || {};
+        window.pie.Controller = Controller;`
+      }
+
+      logger.silly('now use supportModules to prep the webpack config: ', JSON.stringify(supportConfig));
+      let libs = _.flatten([pieController, 'pie-player'].concat(_.map(this._question.pies, 'name')));
+      logger.silly('[buildElementBundle] libs: ', libs);
+      return elementBundle.build(this._question.dir, libs, opts.pieJs, supportConfig.webpackLoaders.bind(supportConfig))
+    };
+
+
+    return this._npmDir.install(npmDependencies)
+      .then(() => {
+        let buildKeys = this._question.buildKeys;
+        let supportConfig = this._frameworkSupport.buildConfigFromKeys(buildKeys);
+        logger.silly('supportDependencies: ', JSON.stringify(supportConfig.npmDependencies));
+        return this._npmDir.installMoreDependencies(supportConfig.npmDependencies)
+         .then(() => supportConfig);
+      })
+      .then(buildElementBundle)
+      .then(() => controllerMap.build(this._question.dir, opts.configFile, opts.controllersJs, this._question.npmDependencies))
+      .then(() => {
+        if (!opts.keepBuildAssets) {
+          return this._npmDir.clean()
+            .then(() => elementBundle.cleanBuildAssets(this._question.dir));
+        } else {
+          return Promise.resolve();
+        }
+      })
+      .then(() => {
+        if (opts.buildExample) {
+          return markupExample.build(this._question.dir, opts.markupFile, opts.exampleFile, opts.configFile);
+        } else {
+          return Promise.resolve('');
+        }
+      })
+      .then(() => logger.debug('packing completed'));
+  }
+}
+
+export const DEFAULT_DEPENDENCIES = {
+  'pie-player': 'PieLabs/pie-player',
+  'pie-controller': 'PieLabs/pie-controller',
+  'babel-core': '^6.0.0',
+  'webpack': '^2.1.0-beta',
+  'babel-loader': '^6.2.5',
+  'babel-preset-es2015': '^6.14.0',
+};
 
 export const DEFAULTS = {
   configFile: 'config.json',
@@ -20,98 +110,4 @@ export const DEFAULTS = {
   keepBuildAssets: false,
   pieJs: 'pie.js',
   controllersJs: 'controllers.js'
-};
-
-export function clean(root, opts){
-
-  opts = _.extend({}, DEFAULTS, opts);
-
-  logger.info('clean...', root);
-  let npmDir = new NpmDir(root);
-  return npmDir.clean()
-      .then(() => elementBundle.clean(root, opts.pieJs)) 
-      .then(() => controllerMap.clean(root, opts.controllersJs))
-      .then(() => markupExample.clean(root, opts.exampleFile));
-}
-
-export function build(root, opts, frameworkSupport){
-  logger.info('build...', root);
-
-  opts = _.extend({}, DEFAULTS, opts);
-  
-  logger.silly('build:opts: ', opts);
-  
-  let npmDir = new NpmDir(root);
-  let rawConfig = fs.readJsonSync(path.join(root, opts.configFile));
-  let lookup = fs.readJsonSync(path.join(root, opts.dependenciesFile)) || {};
-  let config = new Config(rawConfig, lookup);
-
-  let npmDependencies = _.extend({}, config.npmDependencies, {
-    'pie-player': 'PieLabs/pie-player',
-    'pie-controller' : 'PieLabs/pie-controller',
-    'babel-core' : '^6.0.0',
-    'webpack' : '^2.1.0-beta',
-    'babel-loader' : '^6.2.5',
-    'babel-preset-es2015' : '^6.14.0',
-    // 'babel-preset-react' : '^6.11.1'
-  });
-
-  // -> do the install then get the dependency tree
-
-  let pieNames = _.keys(config.npmDependencies);
-
-  let supportModules;
-
-  /**
-   * Add dependencies need to build any frameworks used in the package (ie: react,..)
-   * @param tree - a map of depth-0 dependencies of the package - passed to frameworkSupport
-   */
-  let addFrameworkSupportDependencies = (tree) => {
-    logger.silly('dependencyTree', JSON.stringify(tree));
-    supportModules = frameworkSupport.load(tree);
-    let additionalDependencies = supportModules.npmDependencies;
-    logger.silly('additionalDependencies: ', additionalDependencies);
-    return npmDir.installMoreDependencies(additionalDependencies, {save: true})
-      .then(() => supportModules)
-  };
-
-  /**
-   * Build the element bundle.
-   * Include the pie-player and pie-controller packages.
-   */
-  let buildElementBundle = (supportModules) => {
-      let pieController = {
-        key: 'pie-controller',
-        initSrc: `
-        import Controller from 'pie-controller';
-        window.pie = window.pie || {};
-        window.pie.Controller = Controller;`
-      }
-      
-      logger.silly('now use supportModules to prep the webpack config: ', JSON.stringify(supportModules));
-      let libs = _.flatten([pieController,'pie-player'].concat(pieNames));
-      return elementBundle.build(root, libs, opts.pieJs, supportModules.webpackLoaders)
-  };
-
-  return npmDir.install(npmDependencies)
-    .then(() => npmDir.ls())
-    .then(addFrameworkSupportDependencies) 
-    .then(buildElementBundle)
-    .then(() => controllerMap.build(root, opts.configFile, opts.controllersJs)) 
-    .then(() => {
-      if(!opts.keepBuildAssets){
-        return npmDir.clean()
-          .then(() => elementBundle.cleanBuildAssets(root));
-      } else {
-        return Promise.resolve();
-      }
-    }) 
-    .then(() => { 
-      if(opts.buildExample){
-        return markupExample.build(root, opts.markupFile, opts.exampleFile, opts.configFile);
-      } else {
-        return Promise.resolve('');
-      }
-    })
-    .then(() => logger.debug('packing completed'));
 }
