9fe2e3826e618c18b234537a07edbdf18ae7267b
edeustace <ed.eustace@gmail.com>
2016-10-17 19:24:28 +0100

    add sub lib dirs

---
+++ test/integration/example-components/vue-component/lib/component.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>I am a vue component here is my <code>msg</code>:
+	<span class="example">{{msg}}</span></div>
+</template>
+<script>
+export default {
+  props: { msg : {type: String, default: 'default' }}
+}
+</script>
+
+<style>
+	.example {
+		color: red;
+	}
+</style>
