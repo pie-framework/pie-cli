ee07d56609fcdb3e4ea2cda476297f200d6de485
Attila Miklosi <attila.miklosi@gmail.com>
2016-10-25 08:22:28 +0200

    inject controller into control panel

--- src/code-gen/markup-example.js
+++ src/code-gen/markup-example.js
@@ -25,7 +25,8 @@ let mkExampleMarkup = (markup, model, controllerFile, controllerUid) => `
           player.session = window.pie.session;
           
           var panel = document.querySelector('pie-control-panel');
-          panel.env = window.pie.env; 
+          panel.env = window.pie.env;
+          panel.controller = pieController;
           
           panel.addEventListener('envChanged', function(event){
             console.log('envChanged', event.target.env);
