1669621b80261726c614cc7e6c7905f851c7bcdc
edeustace <ed.eustace@gmail.com>
2016-10-28 20:39:23 +0100

    glue in webpackDevMiddleware

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,8 @@
 import { buildLogger } from '../log-factory';
 import express from 'express';
 import { join } from 'path';
+import webpack from 'webpack';
+import webpackMiddleware from 'webpack-dev-middleware';
 
 const logger = buildLogger();
 
@@ -10,6 +12,14 @@ export function make(configs, controllersUid) {
   const app = express();
   app.set('views', join(__dirname, 'views'));
   app.set('view engine', 'pug');
+
+  app.use(webpackMiddleware(webpack(configs.client), {
+    publicPath: '/'
+  }));
+
+  app.use(webpackMiddleware(webpack(configs.controllers), {
+    publicPath: '/'
+  }));
   // respond with "hello world" when a GET request is made to the homepage
   app.get('/', function (req, res) {
     res.render('example', { title: 'hi', message: 'message...' });
