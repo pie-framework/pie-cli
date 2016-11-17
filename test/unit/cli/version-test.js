4aec17b3403cbc5c64c4a7d9e1b26a448746666e
edeustace <ed.eustace@gmail.com>
2016-11-17 09:33:07 +0000

    fix(cli): dont add git-sha if pie packages isn't in a git dir
    Closes: #42

---
+++ test/unit/cli/version-test.js
@@ -0,0 +1,46 @@
+import proxyquire from 'proxyquire';
+import {assert, stub} from 'sinon';
+
+describe('version', () => {
+  
+  let cmd, log, gitExists;
+
+  let getCmd = () => {
+    return proxyquire('../../../src/cli/version', {
+      'fs-extra' : {
+        readJsonSync: stub().returns({version: '1.0.0'}),
+        existsSync: stub().returns(gitExists)
+      },
+      'child_process' : {
+        execSync: stub().returns('HASH')
+      }
+    }); 
+  }
+
+  beforeEach(() => {
+    log = stub();
+    gitExists = false;
+    process.exit = stub();
+    cmd = getCmd();
+  });
+
+
+  it('does not add git-sha if .git dir doesnt exist', () => {
+    cmd.run(log);
+    assert.calledWith(log, 'version: 1.0.0');
+  });
+
+  describe('if .git dir exists', () => {
+
+    beforeEach(() => {
+      gitExists = true;
+      cmd = getCmd();
+    });
+
+    it('does add git-sha if .git dir exists', () => {
+      cmd.run(log);
+      assert.calledWith(log, 'version: 1.0.0, git-sha: HASH');
+    });
+
+  }); 
+});
