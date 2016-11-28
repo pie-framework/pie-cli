19a232f46e1531166fa30e7ac89fe41e0ba24968
edeustace <ed.eustace@gmail.com>
2016-11-28 11:33:58 +0000

    set up dev

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,14 @@
 const gulp = require('gulp'),
-  babel = require('gulp-babel'),
-  gutil = require('gulp-util'),
-  eslint = require('gulp-eslint'),
-  sourcemaps = require('gulp-sourcemaps'),
   mocha = require('gulp-mocha'),
   releaseHelper = require('release-helper'),
   ts = require('gulp-typescript'),
   tsProject = ts.createProject('tsconfig.json');
 
-
-
-
-
 //Init custom release tasks
 releaseHelper.init(gulp);
 
 let glue = suffix => gulp.src(`src/**/*.${suffix}`).pipe(gulp.dest('lib'));
+
 let watch = (suffix, tasks) => {
   tasks = tasks ? tasks : [suffix];
   return gulp.watch(`src/**/*.${suffix}`, tasks);
@@ -25,23 +18,16 @@ gulp.task('pug', () => glue('pug'));
 gulp.task('md', () => glue('md'));
 gulp.task('ejs', () => glue('ejs'));
 
-gulp.task('typescript', () => {
+gulp.task('ts', () => {
   let tsResult = tsProject.src()
     .pipe(tsProject());
   return tsResult.js.pipe(gulp.dest('lib'));
 });
 
-
+gulp.task('watch-ts', () => watch('ts'));
 gulp.task('watch-pug', () => watch('pug'));
 gulp.task('watch-ejs', () => watch('ejs'));
 gulp.task('watch-md', () => watch('md'));
-
-gulp.task('lint', () => {
-  return gulp.src(['src/**/*.js', '!node_modules/**'])
-    .pipe(eslint())
-    .pipe(eslint.format())
-    .pipe(eslint.failAfterError());
-});
 
 gulp.task('unit', ['build'], () => {
   return gulp.src('test/unit/**/*.js', { read: false })
@@ -53,8 +39,8 @@ gulp.task('it', ['build'], () => {
     .pipe(mocha({ require: ['babel-register'] }));
 });
 
-gulp.task('build', ['lint', 'md', 'ejs', 'pug', 'typescript']);
+gulp.task('build', ['md', 'ejs', 'pug', 'ts']);
 
-gulp.task('dev', ['md', 'pug', 'typescript', 'watch-md', 'watch-ejs', 'watch-pug', 'watch-babel']);
+gulp.task('dev', ['build', 'watch-md', 'watch-ejs', 'watch-pug', 'watch-ts']);
 
 gulp.task('test', ['unit']);
