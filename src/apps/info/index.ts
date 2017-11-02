9474508c8c8f0e5fa9b58459d516a9f94cd57cf9
edeustace <ed.eustace@gmail.com>
2017-11-02 12:39:33 -0400

    clean up

--- src/apps/info/index.ts
+++ src/apps/info/index.ts
@@ -80,51 +80,6 @@ export default class InfoApp implements App, Servable {
       return match;
     });
 
-    /*
-    // const DashboardPlugin = require('webpack-dashboard/plugin');
-
-module.exports = {
-  devtool: 'cheap-eval-source-map',
-  context: __dirname,
-  entry: './entry.jsx',
-  output: {
-    filename: 'bundle.js',
-    path: __dirname
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.jsx$/,
-        loader: 'babel-loader',
-        options: {
-          babelrc: false,
-          presets: [
-            'react', 'es2015', 'stage-0'
-
-          ]
-        }
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      },
-      {
-        test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2|otf)$/,
-        loader: 'url-loader',
-        options: {
-          limit: 10000
-        }
-      }
-    ]
-  },
-  resolve: {
-    extensions: ['.js', '.jsx']
-  },
-  plugins: [
-    // new DashboardPlugin()
-  ]
-}
-    */
     cssRule.exclude = [
       /.*highlight\.js.*/,
     ];
